5637438f809194995961af1ff3d7a27620651bbb
nathan74490 <na.boudet@formation-cci.fr>
2025-02-04 16:01:33 +0100

    jkfhkg

--- src/Assurance.vue
+++ src/Assurance.vue
@@ -1,5 +1,12 @@
 <template>
-    <h1>Assurance</h1>
+    <label for="yesOrNo">Assuré:</label>
+    <select name="yesOrNo" id="yesOrNo">
+        <option value="">Non</option>
+        <option value="">Oui</option>
+    </select>
+    <label for="DateFinAssurance"> quel est la date de fin de votre protection par l'assurance</label>
+    <input type="date" id="DateFinAssurance" value="2018-07-22" min="2025-01-01" max="2026-12-31" >
+    
 </template>
 <script></script>
 <style></style>
--- src/Entretien.vue
+++ src/Entretien.vue
@@ -1,3 +1,6 @@
-<template><H1>Entretien</H1></template>
+<template><H1>Entretien</H1>
+<label for="dateFinCont">quel est la date de fin de validiter de votre </label>
+<input type="text" id="dateFinCont" placeholder="votre reponse">
+</template>
 <script></script>
 <style></style>
